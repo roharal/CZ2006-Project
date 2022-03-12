b0d6e9c7a32d7b8dd5c2eeb8755dca891b276f25
deionna <deionnachee26@gmail.com>
2022-03-13 03:09:42 +0800

    doneee

--- exploresg/lib/screens/aftersearch.dart
+++ exploresg/lib/screens/aftersearch.dart
@@ -2,23 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:exploresg/helper/utils.dart';
 import 'package:exploresg/helper/placewidgets.dart';
 
-class HomeScreen extends StatefulWidget {
+class AfterSearchScreen extends StatefulWidget {
+  const AfterSearchScreen({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() {
-    return _HomeScreen();
-  }
+  State<AfterSearchScreen> createState() => _AfterSearchState();
 }
 
-class _HomeScreen extends State<HomeScreen> {
-  // _moveToAnotherPageWithinNavBar() {
-  //   pushNewScreenWithRouteSettings(
-  //       context,
-  //       screen: PlaceScreen(), // class you're moving into
-  //       settings: RouteSettings(name: PlaceScreen.routeName), // remember to include routeName at the base class
-  //       pageTransitionAnimation: PageTransitionAnimation.cupertino
-  //   );
-  // }
-
+class _AfterSearchState extends State<AfterSearchScreen> {
   @override
   Widget build(BuildContext context) {
     Place testplace = Place(
@@ -26,6 +17,7 @@ class _HomeScreen extends State<HomeScreen> {
         "fun place fun place fun place fun place fun place fun place fun place fun place fun place fun place fun placfun place fun place fun place fun place fun place fun place fun place fun place fun place fun place fun placfun place fun place fun place fun place fun place fun place fun place fun place fun place fun place fun placfun place fun place fun place fun place fun place fun place fun place fun place fun place fun place fun place fun place ",
         "Singapore 512345 Happy Road",
         4);
+
     List<Place> places = [
       testplace,
       testplace,
@@ -41,25 +33,21 @@ class _HomeScreen extends State<HomeScreen> {
     return Scaffold(
         backgroundColor: createMaterialColor(Color(0xFFFFF9ED)),
         body: Container(
-          child: SingleChildScrollView(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              topBar("home", height, width, 'assets/img/homeTop.png'),
+            child: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+              topBar("places", height, width, 'assets/img/afterSearchTop.png'),
               SizedBox(height: 10),
-              textMajor("find places", Colors.black, 26),
               Search(width: 0.80 * width, height: 0.3 * height),
-              Image.asset("assets/img/stringAccent.png"),
-              textMajor("explore", Colors.black, 26),
+              SizedBox(height: 20),
               for (place in places)
                 PlaceContainer(
                     place: place, width: 0.8 * width, height: 0.3 * height),
               SizedBox(
                 height: 60,
               ),
-            ],
-          )),
-        ));
+            ]))));
   }
 }
