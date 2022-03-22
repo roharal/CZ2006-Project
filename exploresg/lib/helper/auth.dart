328c50bac55bdae0163e61a8e057afc226c73979
Herman <hermanfyh@gmail.com>
2022-03-22 12:44:37 +0800

    Added user functions

--- exploresg/lib/helper/auth.dart
+++ exploresg/lib/helper/auth.dart
@@ -3,6 +3,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 class Auth {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
+  User? getCurrentUser() {
+    return auth.currentUser;
+  }
+
+  Future<void> logOut() {
+    return auth.signOut();
+  }
+
   Future<User> signInEmail(String email, String password) async {
     UserCredential result = await auth.signInWithEmailAndPassword(email: email, password: password);
     final User user = result.user!;
