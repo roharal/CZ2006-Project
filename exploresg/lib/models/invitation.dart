d7321962a117abdf9005339be10dbdfb06b06015
Herman <hermanfyh@gmail.com>
2022-03-31 14:48:38 +0800

    Minor changes

--- exploresg/lib/models/invitation.dart
+++ exploresg/lib/models/invitation.dart
@@ -1,10 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:exploresg/models/place.dart';
+import 'package:exploresg/models/user.dart';
 
 class Invitation {
-  late Place place;
-  late DateTime inviteTime;
-  late var inviter;
-  late bool accepted = false;
+  Place place;
+  DateTime dateTime;
+  List<UserModel> users;
 
-  Invitation(this.place,this.inviteTime,this.accepted,this.inviter);
+
+  Invitation(this.place,this.dateTime,this.users);
+
+  Place getPlace() {
+    return this.place;
+  }
+
+  DateTime getDateTime() {
+    return this.dateTime;
+  }
+
+  List<UserModel> getUsers() {
+    return this.users;
+  }
+
+  factory Invitation.fromSnapshot(DocumentSnapshot snapshot) {
+    var users = snapshot["users"] as List;
+    List<UserModel> userModels = users.map((i) => UserModel.fromSnapshot(i)).toList();
+    return Invitation(
+      snapshot["place"],
+      snapshot["dateTime"],
+      userModels
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    "place": place,
+    "datetime": dateTime,
+    "users": users.map((i) => i.toJson()).toList()
+  };
 }
