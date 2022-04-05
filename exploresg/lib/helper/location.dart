081d5e13eb1f44ac9d67b866570623b7c10cfef4
Herman <hermanfyh@gmail.com>
2022-04-05 23:49:39 +0800

    Added permission checker

--- exploresg/lib/helper/location.dart
+++ exploresg/lib/helper/location.dart
@@ -4,8 +4,30 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class Locator {
   var geoLocator = Geolocator();
 
-  Future<LatLng> getLongLang() async {
+  Future<LatLng?> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // return Future.error('Location services are disabled.');
+      return null;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // return Future.error('Location permissions are denied');
+        return null;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+      return null;
+    }
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     return LatLng(position.latitude,position.longitude);
   }
+
+
 }
