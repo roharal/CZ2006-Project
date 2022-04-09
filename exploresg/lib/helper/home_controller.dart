d677dddf2af0d9ed7909adc1482c015d7e527daa
Herman <hermanfyh@gmail.com>
2022-04-09 23:56:28 +0800

    Fixed and renamed

---
+++ exploresg/lib/helper/home_controller.dart
@@ -0,0 +1,48 @@
+import 'package:exploresg/helper/location.dart';
+import 'package:exploresg/helper/places_api.dart';
+import 'package:exploresg/helper/utils.dart';
+import 'package:exploresg/models/place.dart';
+import 'package:exploresg/helper/authController.dart';
+import 'package:flutter/material.dart';
+
+class HomeController {
+  AuthController _auth = AuthController();
+  PlacesApi _placesApi = PlacesApi();
+
+  Future<List<Place>?> loadRecommendations(BuildContext context) async {
+    String uid = _auth.getCurrentUser()!.uid;
+    String interest = "";
+    Locator locator = new Locator();
+    var coor = await locator.getCurrentLocation();
+    if (coor != null) {
+      List<Place> _mixPlaces = [];
+      await _auth.getUserFromId(uid).then((value) {
+        interest = value["interest"];
+      }).onError((error, stackTrace) {
+        showAlert(context, "Retrieve User Profile", error.toString());
+      });
+      if (interest != "") {
+        var split = interest.split(",");
+        for (String s in split) {
+          var result = await _placesApi.nearbySearchFromText(
+              coor.latitude.toString(),
+              coor.longitude.toString(),
+              10000,
+              "&type=$s");
+          for (var i in result!) {
+            _mixPlaces.add(i);
+          }
+        }
+        _mixPlaces = (_mixPlaces..shuffle());
+        while (_mixPlaces.length > 5) {
+          _mixPlaces.removeLast();
+        }
+      }
+      return _mixPlaces;
+    } else {
+      showAlert(context, "Location Permission Error",
+          "Location permission either disable or disabled. Please enable to enjoy the full experience.");
+      return null;
+    }
+  }
+}
